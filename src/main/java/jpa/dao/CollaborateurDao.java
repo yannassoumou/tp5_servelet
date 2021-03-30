85bb38db98ce211beec78f43e2ea4144f827109f
Ehuié <ehuie.manvoule@etudiant.univ-rennes1.fr>
2021-03-30 02:22:19 +0200

    Test requete

--- src/main/java/jpa/dao/CollaborateurDao.java
+++ src/main/java/jpa/dao/CollaborateurDao.java
@@ -1,4 +1,15 @@
 package jpa.dao;
 
-public class CollaborateurDao {
+import jpa.business.Collaborateur;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface CollaborateurDao extends JpaRepository<Collaborateur, String> {
+
+    @Query("select c from Collaborateur c where  c.type_collaborateur = :type")
+    public List<Collaborateur> findByTypeCollaborateur (@Param("type") String type) ;
+
 }
